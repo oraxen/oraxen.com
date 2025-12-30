069bea9773151df2439aa31f9c8308376a27aa90
Thomas Marchand <thomas.marchand@tuta.io>
2025-12-30 10:23:43 +0100

    fix: minify GA inline script to avoid newline issues

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -19,12 +19,7 @@ export function GoogleAnalytics() {
         id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}');
-          `,
+          __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_MEASUREMENT_ID}');`,
         }}
       />
     </>
